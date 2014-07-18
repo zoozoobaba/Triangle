76d8231c701d552e31dacc0feacf5995adfe2705
Ruben Rhodes <zoozoobaba@gmail.com>
2014-07-17 20:52:45 -0700

    Invalid triangles added

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ var triangle = function(side1, side2, side3) {
   if (side1 === side2 && side1 === side3) {
     return "Equilateral triangle"
   };
+
+  if (side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)) {
+    return "Not a triangle";
+  };
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,9 @@
 describe("triangle", function() {
-  it("returns 'Equilateral Triangle' if all three sides are the same", function() {
+  it("returns 'Equilateral triangle' if all three sides are the same", function() {
     triangle(3,3,3).should.equal("Equilateral triangle");
+  });
+
+  it("returns 'Not a triangle' if one side is greater than the other two sides combined", function() {
+    triangle(2,2,9).should.equal('Not a triangle');
   });
 });
