722da3d61e4d919b7cd8fb46d14e40b92655c8b3
Ruben Rhodes <zoozoobaba@gmail.com>
2014-07-17 22:04:49 -0700

    Web page create

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ var triangle = function(side1, side2, side3) {
     return "Scalene triangle";
   };
 }; 
+
+
+
+$(document).ready(function(){
+  $("form#triangle-form").submit(function(event) {
+    event.preventDefault();
+    var side1 = $("#side1").val()
+    var side2 = $("#side2").val()
+    var side3 = $("#side3").val()
+    $("#result").text(triangle(parseInt(side1), parseInt(side2), parseInt(side3)));
+  });
+});
