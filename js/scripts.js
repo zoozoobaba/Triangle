64919c47bb8bfc7c8e0cd4da1b25ab6edd3266e7
Ruben Rhodes <zoozoobaba@gmail.com>
2014-07-17 21:29:50 -0700

    Scalene added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 var triangle = function(side1, side2, side3) {
   if (side1 === side2 && side1 === side3) {
-    return "Equilateral triangle"
-  } else if (side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)) {
+    return "Equilateral triangle";
+  } else if (side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)) {
     return "Not a triangle";
   } else if (side1 === side2 || side1 === side3 || side2 === side3) {
     return "Isosceles triangle";
+  } else {
+    return "Scalene triangle";
   };
 }; 
--- specs/specs.js
+++ specs/specs.js
@@ -3,11 +3,15 @@ describe("triangle", function() {
     triangle(3,3,3).should.equal("Equilateral triangle");
   });
 
-  it("returns 'Not a triangle' if one side is greater than the other two sides combined", function() {
+  it("returns 'Not a triangle' if one side is greater or equal to the other two sides combined", function() {
     triangle(2,2,9).should.equal("Not a triangle");
   });
 
   it("returns 'Isosceles triangle' if two sides are the same", function() {
     triangle(7,7,8).should.equal("Isosceles triangle");
+  });
+
+  it("returns 'Scalene triangle' if all sides are different", function() {
+    triangle(6,7,8).should.equal("Scalene triangle");
   });
 });
